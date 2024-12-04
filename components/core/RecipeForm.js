05635a7ab2136548952b7ad8cec4f3b9f0d06f97
Luis Castro <foreverluiscastro@gmail.com>
2024-12-03 19:34:03 -0500

    finishing touches.

--- components/core/RecipeForm.js
+++ components/core/RecipeForm.js
@@ -49,7 +49,7 @@ export default function RecipeForm() {
     <div className="flex flex-col bg-sky-200 bg-opacity-80 shadow-md rounded-xl h-full py-8 ">
       <div className="flex flex-col px-8 w-full text-left">
         <h1 className="font-semibold text-2xl mb-4">Generate a recipe!</h1>
-        <p className="text-xl mb-4 bg-sky-700 bg-opacity-80 rounded-lg p-4">Add any ingredients you have available as well as the style you're looking for and Remy will generate a great recipe!</p>
+        <p className="text-xl mb-4 bg-sky-700 bg-opacity-80 rounded-lg p-4">Add any ingredients you have available as well as the style you are looking for and Remy will generate a great recipe!</p>
         <div className="flex flex-col w-full">
         
           <label className="mb-2 font-semibold text-xl">Ingredients</label>
